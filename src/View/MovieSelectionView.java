ae14de6c683964b45c54e345c887afb7990bec96
GraydonHall42 <gwhall@ualberta.ca>
2021-12-01 17:14:17 -0700

    create movie selection view

---
+++ src/View/MovieSelectionView.java
@@ -0,0 +1,221 @@
+package View;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MovieSelectionView extends JFrame{
+    private JTextField tfStudentName;
+    private JTextField tfStudentID;
+    private JButton purchaseTicketButton;
+    private JButton logInButton;
+    private JPanel mainPanel;
+    private JButton accountButton;
+
+    private JLabel movieLabel;
+    private JComboBox movieSelectorComboBox;
+    private JComboBox theatreSelectionComboBox;
+    private JLabel showtimeLabel;
+    private JComboBox showtimeSelectorComboBox;
+    private JPanel seatPanel;
+
+    private JLabel screenLabel;
+
+
+    // menu options
+    String[] movieOptions = {};
+    String[] theatreOptions = {};
+    String[] timeOptions = {};
+
+    JButton[][] seats;
+    boolean loggedIn;
+
+    public MovieSelectionView(){
+
+        // seats array
+        seats = new JButton[10][10];
+
+        // sets up our dropdown menu
+        dropdownMenuSetup();
+
+        // create all seats (start off as disabled)
+        createSeats();
+
+        // set logged in as false to start off
+        setLoggedIn(false);
+
+        setContentPane(mainPanel);
+        setTitle("Movie Selection Menu");
+        setSize(450,500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        seatPanel.setLayout(new BoxLayout(seatPanel, BoxLayout.PAGE_AXIS));
+
+
+        setVisible(true);
+        this.setSize(new Dimension(720,520));
+
+    }
+
+    private void dropdownMenuSetup(){
+        // setup dropdown menus
+        movieSelectorComboBox.setModel(new DefaultComboBoxModel(movieOptions));
+        movieSelectorComboBox.setFocusable(false);
+        movieSelectorComboBox.setSelectedIndex(-1);
+        movieSelectorComboBox.setEnabled(false);
+
+        theatreSelectionComboBox.setModel(new DefaultComboBoxModel(theatreOptions));
+        theatreSelectionComboBox.setFocusable(false);
+        theatreSelectionComboBox.setSelectedIndex(-1);
+        theatreSelectionComboBox.setEnabled(false);
+
+        showtimeSelectorComboBox.setModel(new DefaultComboBoxModel(timeOptions));
+        showtimeSelectorComboBox.setFocusable(false);
+        showtimeSelectorComboBox.setSelectedIndex(-1);
+        showtimeSelectorComboBox.setEnabled(false);
+    }
+
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+
+        if (loggedIn){
+            logInButton.setText("Log Out");
+            accountButton.setText("Account Settings");
+        } else{
+            logInButton.setText("Log In");
+            accountButton.setText("Sign Up");
+        }
+    }
+
+    public void createSeats() {
+        int width = 60;
+        int height = 30;
+
+        for(int i=0; i<10; i++){
+            var x = new JPanel();
+            x.setLayout(new BoxLayout(x, BoxLayout.LINE_AXIS));
+            for(int j=0; j<10; j++){
+                JButton btn = new JButton(""+ (char)(i+65) + (j));
+                btn.setEnabled(false);
+                btn.setMinimumSize(new Dimension(width, height));
+                btn.setMaximumSize(new Dimension(width, height));
+
+                btn.addActionListener(new buttonListener());
+
+                x.add(btn);
+                seats[i][j]=btn;
+            }
+            seatPanel.add(x);
+        }
+        this.setVisible(true);
+    }
+
+    class buttonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            var x = (JButton) e.getSource();
+            if(x.getBackground()==Color.green){
+                x.setBackground(null);
+            }else{
+                x.setBackground(Color.green);
+            }
+
+        }
+    }
+
+    public void disableAllSeats(){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                seats[i][j].setEnabled(false);
+                seats[i][j].setBackground(null);
+            }
+        }
+    }
+
+    public void setSeats(int[][] showtimeSeats) {
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                seats[i][j].setEnabled(showtimeSeats[i][j]==1);
+            }
+        }
+        this.setVisible(true);
+    }
+
+    public JButton[][] getSeats() {
+        return seats;
+    }
+
+    public void setTheatreOptions(String[] theatreOptions) {
+        this.theatreOptions = theatreOptions;
+        theatreSelectionComboBox.setModel(new DefaultComboBoxModel(theatreOptions));
+        theatreSelectionComboBox.setSelectedIndex(-1);
+        theatreSelectionComboBox.setEnabled(true);
+    }
+
+    public void setTimeOptions(String[] timeOptions) {
+        this.timeOptions = timeOptions;
+        showtimeSelectorComboBox.setModel(new DefaultComboBoxModel(timeOptions));
+        showtimeSelectorComboBox.setSelectedIndex(-1);
+        showtimeSelectorComboBox.setEnabled(true);
+    }
+
+    public void clearShowtimeOptions(){
+        this.timeOptions = new String[]{};
+        showtimeSelectorComboBox.setModel(new DefaultComboBoxModel(timeOptions));
+        showtimeSelectorComboBox.setSelectedIndex(-1);
+        showtimeSelectorComboBox.setEnabled(false);
+    }
+
+    public void setMovieOptions(String[] movieOptions) {
+        this.movieOptions = movieOptions;
+        movieSelectorComboBox.setModel(new DefaultComboBoxModel(movieOptions));
+        movieSelectorComboBox.setSelectedIndex(-1);
+        movieSelectorComboBox.setEnabled(true);
+    }
+
+    // get user selections
+    public String getMovieInput(){
+        return (movieSelectorComboBox.getSelectedItem() == null) ? "null" : movieSelectorComboBox.getSelectedItem().toString();
+    }
+
+    public String getTheatreInput(){
+        return (theatreSelectionComboBox.getSelectedItem() == null) ? "null" : theatreSelectionComboBox.getSelectedItem().toString();
+    }
+
+    public String getShowtimeInput(){
+        return (showtimeSelectorComboBox.getSelectedItem() == null) ? "null" : showtimeSelectorComboBox.getSelectedItem().toString();
+    }
+
+    // add and remove action listeners
+    public void addMovieComboBoxActionListener(ActionListener a){
+        movieSelectorComboBox.addActionListener(a);
+    }
+
+    public void removeMovieComboBoxActionListener(ActionListener a){movieSelectorComboBox.removeActionListener(a);}
+
+    public void addTheatreComboBoxActionListener(ActionListener a){
+        theatreSelectionComboBox.addActionListener(a);
+    }
+
+    public void removeTheatreComboBoxActionListener(ActionListener a){theatreSelectionComboBox.removeActionListener(a);}
+
+
+    public void addShowtimeComboBoxActionListener(ActionListener a){
+        showtimeSelectorComboBox.addActionListener(a);
+    }
+
+    public void removeShowtimeComboBoxActionListener(ActionListener a){
+        showtimeSelectorComboBox.removeActionListener(a);
+    }
+
+    public void addPurchaseButtonActionListener(ActionListener a){
+        purchaseTicketButton.addActionListener(a);
+    }
+
+    public void setScreenLabelText(String s) {
+        this.screenLabel.setText(s);
+    }
+
+}
---
+++ src/movieTicketSystem/controller/ViewController.java
@@ -0,0 +1,154 @@
+package movieTicketSystem.controller;
+
+import View.MovieSelectionView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ViewController {
+
+    MovieSelectionView theView;
+    movieComboBoxListener movieListener;
+    theatreComboBoxListener theatreListener;
+    showtimeComboBoxListener showtimeListener;
+    purchaseButtonListener purchaseButtonListener;
+
+
+    public ViewController(MovieSelectionView theView) {
+        this.theView = theView;
+        movieListener = new movieComboBoxListener();
+        theatreListener = new theatreComboBoxListener();
+        showtimeListener = new showtimeComboBoxListener();
+        purchaseButtonListener = new purchaseButtonListener();
+
+        // **** TALK TO BACK END AND GET INITIAL MOVIE LIST ****
+        String[] movieOptions = {
+                "Knives Out",
+                "House of Gucci",
+                "Lord of the Rings"
+        };
+        theView.setMovieOptions(movieOptions);
+
+        // add action listeners
+        theView.addMovieComboBoxActionListener(movieListener);
+        theView.addTheatreComboBoxActionListener(theatreListener);
+        theView.addShowtimeComboBoxActionListener(showtimeListener);
+        theView.addPurchaseButtonActionListener(purchaseButtonListener);
+
+        theView.setVisible(true);
+
+    }
+
+    class movieComboBoxListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // **** TALK TO BACK END AND GET LIST OF THEATRES PLAYING THIS MOVIE ****
+            System.out.println("Movie Selected: "  + theView.getMovieInput());
+            String[] theatreOptions = {
+                    "Scotiabank Theatre Chinook",
+                    "Canyon Meadows Cinema",
+                    "Shawnessey Theatre"
+            };
+
+            // clear any previous showtime options
+            theView.removeShowtimeComboBoxActionListener(showtimeListener);
+            theView.clearShowtimeOptions();
+            theView.addShowtimeComboBoxActionListener(showtimeListener);
+
+            // clear previously selected seats
+            theView.disableAllSeats();
+
+            // add theatre options based on movie selected
+            theView.removeTheatreComboBoxActionListener(theatreListener);
+            theView.setTheatreOptions(theatreOptions);
+            theView.addTheatreComboBoxActionListener(theatreListener);
+
+        }
+    }
+
+    class theatreComboBoxListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // *** TALK TO BACK END AND GET LIST OF SHOWTIMES FOR THIS MOVIE/THEATRE ****
+            System.out.println("Theatre Selected: "  + theView.getTheatreInput());
+            String[] timeOptions = {
+                    "1:00 pm",
+                    "3:00 pm",
+                    "5:00 pm",
+                    "7:00 pm",
+            };
+
+            //disable any previously selected seats for a different showtime
+            theView.disableAllSeats();
+
+            // add showtimes for this theatre / movie combo
+            theView.removeShowtimeComboBoxActionListener(showtimeListener);
+            theView.setTimeOptions(timeOptions);
+            theView.addShowtimeComboBoxActionListener(showtimeListener);
+
+        }
+    }
+
+    class showtimeComboBoxListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            // *** TALK TO BACK END AND GET ARRAY OF SEATS FOR THIS SHOWTIME/MOVIE/THEATRE
+            System.out.println("Showtime Selected: "  + theView.getShowtimeInput());
+            int[][] seats = {
+                    {0, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            };
+
+            theView.setSeats(seats);}
+    }
+
+    class purchaseButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            //**** FOR EACH SELECTED SEAT, SEND ROW, COL, MOVIE, THEATRE, SHOWTIME TO BACK END FOR PURCHASE ****
+            JButton[][] seats = theView.getSeats();
+            for(int i=0; i<10; i++){
+                for(int j=0; j<10; j++){
+                    Color seatBackground = seats[i][j].getBackground();
+                    if(seatBackground == Color.green){
+                        int row = i;
+                        int col = j;
+                        System.out.println(
+                                "\nPURCHASE INFO:"
+                                + "\n row: " + (i+1)
+                                + "\n col: " + (j+1)
+                                + "\n movie: " + theView.getMovieInput()
+                                + "\n theatre: " + theView.getTheatreInput()
+                                + "\n showtime: " + theView.getShowtimeInput()
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        MovieSelectionView theView = new MovieSelectionView();
+        ViewController x = new ViewController(theView);
+    }
+
+}
