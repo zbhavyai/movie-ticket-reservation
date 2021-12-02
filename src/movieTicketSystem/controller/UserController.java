58f5a721c7521fe74613d3e6681a1ef904fac4af
Bhavyai Gupta <zbhavyai@gmail.com>
2021-12-01 23:59:08 -0700

    User model and controller

---
+++ src/movieTicketSystem/controller/UserController.java
@@ -0,0 +1,44 @@
+package movieTicketSystem.controller;
+
+import java.util.ArrayList;
+
+import movieTicketSystem.model.RegisteredUser;
+
+public class UserController {
+    private ArrayList<RegisteredUser> allRegisteredUsers;
+    private static UserController instanceVar;
+    private DbController db;
+
+    public static UserController getInstance() {
+        if (instanceVar == null) {
+            instanceVar = new UserController();
+            return instanceVar;
+        }
+
+        else {
+            return instanceVar;
+        }
+    }
+
+    private UserController() {
+        this.allRegisteredUsers = new ArrayList<>();
+        this.db = new DbController();
+
+    }
+
+    public RegisteredUser verifyUser(String username, String password) {
+        RegisteredUser ru = this.db.searchRegisteredUser(username);
+
+        if(ru == null) {
+            return null;
+        }
+
+        else if (!ru.getPassword().equals(password)) {
+            return null;
+        }
+
+        else {
+            return ru;
+        }
+    }
+}
---
+++ src/movieTicketSystem/model/RegisteredUser.java
@@ -0,0 +1,113 @@
+package movieTicketSystem.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class RegisteredUser {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+    private List<Ticket> tickets;
+    private List<Receipt> receipts;
+    private Payment card;
+    private LocalDate lastFeePaid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public List<Receipt> getReceipts() {
+        return receipts;
+    }
+
+    public void setReceipts(List<Receipt> receipts) {
+        this.receipts = receipts;
+    }
+
+    public Payment getCard() {
+        return card;
+    }
+
+    public void setCard(Payment card) {
+        this.card = card;
+    }
+
+    public LocalDate getLastFeePaid() {
+        return lastFeePaid;
+    }
+
+    public void setLastFeePaid(LocalDate lastFeePaid) {
+        this.lastFeePaid = lastFeePaid;
+    }
+
+    public void buyTicket() {
+        // TODO
+    }
+
+    public Coupon cancelTicket() {
+        // TODO
+        Coupon c = new Coupon();
+        return c;
+    }
+
+    public boolean checkFeeStatus() {
+        if(this.lastFeePaid.plusYears(1).isAfter(LocalDate.now())) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return ("[" + this.username + ", " + this.email + ", " + this.address + ", " + this.card + ", " + lastFeePaid + "]");
+    }
+}
