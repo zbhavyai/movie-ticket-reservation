c0b2cf67a2e9e3af7fbed097446938fe326ccb84
calvingiese <calvin.giese@ucalgary.ca>
2021-12-07 19:01:26 -0700

    Fixed empty seat grid issue.

--- src/movieTicketSystem/controller/DbController.java
+++ src/movieTicketSystem/controller/DbController.java
@@ -752,11 +752,18 @@ public class DbController {
                     }
                 }
             }
-
         }
 
         catch (SQLException ex) {
             ex.printStackTrace();
+        }
+
+        if(tickets.size() == 0){
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    seatGrid[i][j] = 1;
+                }
+            }
         }
 
         return seatGrid;
